1bd785bc2dcacb5421d82b116ecf1e323ae541bf
Koichi Shiraishi <zchee.io@gmail.com>
2022-07-30 00:13:29 +0900

    compute/metadata/fake: add dot to OnTest suffix
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- compute/metadata/fake/testing.go
+++ compute/metadata/fake/testing.go
@@ -7,5 +7,5 @@ import (
 
 // OnTest reports whether the current state is on test.
 func OnTest() bool {
-	return strings.HasSuffix(os.Args[0], "test")
+	return strings.HasSuffix(os.Args[0], ".test")
 }
