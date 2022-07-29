10f5e3524ca6871a17a3a998ec6a06665e2ee62f
Koichi Shiraishi <zchee.io@gmail.com>
2022-07-30 07:22:44 +0900

    compute/metadata/fake: remove unnecessary structs
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- compute/metadata/fake/handler.go
+++ compute/metadata/fake/handler.go
@@ -10,48 +10,30 @@ import (
 	"github.com/google/safehtml"
 )
 
-type rootResponse struct{}
-
-var _ safehttp.ErrorResponse = rootResponse{}
-
-// Code implements safehttp.ErrorResponse.Code.
-func (rootResponse) Code() safehttp.StatusCode {
-	return safehttp.StatusOK
-}
-
-type notDefinedResponse struct{}
-
-var _ safehttp.ErrorResponse = notDefinedResponse{}
-
-// Code implements safehttp.ErrorResponse.Code.
-func (notDefinedResponse) Code() safehttp.StatusCode {
-	return safehttp.StatusNotFound
-}
-
 func rootHandler(w safehttp.ResponseWriter, _ *safehttp.IncomingRequest) safehttp.Result {
-	return w.WriteError(rootResponse{})
+	return w.WriteError(safehttp.StatusOK)
 }
 
 var projectEnvs = []string{"GOOGLE_CLOUD_PROJECT", "GCP_PROJECT", "GOOGLE_GCP_PROJECT"}
 
-func projectHandler(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+func projectHandler(w safehttp.ResponseWriter, _ *safehttp.IncomingRequest) safehttp.Result {
 	for _, env := range projectEnvs {
 		if proj, ok := os.LookupEnv(env); ok {
 			return w.Write(safehtml.HTMLEscaped(proj))
 		}
 	}
 
-	return w.WriteError(notDefinedResponse{})
+	return w.WriteError(safehttp.StatusNotFound)
 }
 
 var numericProjectEnvs = []string{"GOOGLE_CLOUD_NUMERIC_PROJECT", "GCP_NUMERIC_PROJECT", "GOOGLE_GCP_NUMERIC_PROJECT"}
 
-func numericProjectHandler(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+func numericProjectHandler(w safehttp.ResponseWriter, _ *safehttp.IncomingRequest) safehttp.Result {
 	for _, env := range numericProjectEnvs {
 		if proj, ok := os.LookupEnv(env); ok {
 			return w.Write(safehtml.HTMLEscaped(proj))
 		}
 	}
 
-	return w.WriteError(notDefinedResponse{})
+	return w.WriteError(safehttp.StatusNotFound)
 }
